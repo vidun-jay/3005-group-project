eb41088094b6aa5775cb1faa23a257cef5173d54
Darnell <darnellfoster12@gmail.com>
2023-11-29 19:54:53 -0500

    modified:   server.js

--- server.js
+++ server.js
@@ -110,5 +110,5 @@ app.post('/login', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port http://localhost:${PORT}/login`);
 });
